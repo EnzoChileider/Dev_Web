e855095ea196aa33a5976bdf4bee9bf2e6c67fa7
EnzoChileider <enzolopes22@gmail.com>
2023-05-01 17:37:44 -0300

    web-app

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import { MovieProvider } from "./Context/MovieContext";
+
+import Header from "./Components/Header";
+import MovieList from "./Components/movie-list/movie-list";
+import About from "./Pages/About";
+import MovieDetails from "./Pages/MovieDetails";
+import Planos  from "./Pages/planos"
+
+const App = () => {
+  return (
+    <MovieProvider>
+      <Header />
+      <Routes>
+        <Route path="/" element={<MovieList />} />
+        <Route path="about" element={<About />} />
+        <Route path="planos" element={<Planos />} />
+        <Route path="movies/:id" element={<MovieDetails />} />
+        <Route path='*' element={<h1>Página Não Encontrada!</h1>} />
+      </Routes>
+    </MovieProvider>
+  );
+};
+
+export default App;
